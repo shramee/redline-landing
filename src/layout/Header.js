070311cefe9051e20713ba4451f7100ecc50ce32
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-08-18 20:28:51 +0800

    Header layout

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,10 @@
 const Header = () => <>
+	<a className="dtc v-mid mid-gray link dim w-25" href="#" title="Home">
+		<h1 className="montserrat900 white f3 ma0 v-mid ttu">Redline</h1>
+	</a>
+	<nav className="dtc v-mid w-75 tr">
+		<button>Connect</button>
+	</nav>
 </>;
 
 export default Header;
