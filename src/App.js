24f37981dd65fa3f519c4cb23889366ce964d5e5
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-08-18 20:28:36 +0800

    Base layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from './layout/Header'
+import Home from './pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App min-vh-100 flex flex-column w-100 bg-black">
+      <header className="dt w-100 border-box pa3 ph4-ns bb" style={{borderBottom: '3px solid #e00'}}>
+        <Header />
+      </header>
+
+      <section className='w-100 pa3 ph4-ns'>
+        <Home />
+      </section>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,4 @@
+const Home = () => <article className="">
+</article>;
+
+export default Home;
