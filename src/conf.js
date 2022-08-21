160373f606c88d9bd2af7ad1cd61686214150924
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-08-22 02:44:17 +0800

    Contract calls

--- src/conf.js
+++ src/conf.js
@@ -3,7 +3,6 @@ import tiktok from './assets/tiktok-brands.svg';
 
 export const
 	twitchURL                 = 'https://player.twitch.tv/?channel=redlinedao&parent=' + window.location.host.split( ':' )[0],
-	contract_address          = '0x05f940ac40c4aced54b371a00087e14d793a60813b80180f17cbb585e78ad14d',
 	rewardsLinks              = [
 		[<img height='25' alt='Youtube' src={youtube}/>, '#'],
 		[<img height='25' alt='Tiktok' src={tiktok}/>, '#'],
@@ -11,5 +10,13 @@ export const
 	sponsorLinks              = [
 		['isotropic', '#'],
 	],
-	robotsNFTsContractAddress = '0x044915126b3736c22fc04be736caf0813451617fc562dc727d164361b81358a9';
+	raceTrackImageURI              = 'https://redline.game/a/t00.png';
+
+export const
+	defaultSpacesInRace = 6;
+
+// Contract addresses
+export const
+	robotsNFTsContractAddress = '0x044915126b3736c22fc04be736caf0813451617fc562dc727d164361b81358a9',
+	raceContractAddress       = '0x05f940ac40c4aced54b371a00087e14d793a60813b80180f17cbb585e78ad14d';
 
--- src/services/data.js
+++ src/services/data.js
@@ -1,12 +1,34 @@
-import {robotsNFTsContractAddress} from "../conf";
+import {defaultSpacesInRace, raceContractAddress, robotsNFTsContractAddress} from "../conf";
 import dummyRobotNFTs from './mock/NFTAssets';
 
 export const
 	getRobots = ( walletAddress ) => {
 		const options = {method: 'GET', headers: {Accept: 'application/json'}};
-//		return new Promise( res => res( dummyRobotNFTs ) );
-		return fetch(`https://api-testnet.aspect.co/api/v0/assets?contract_address=${robotsNFTsContractAddress}&owner_address=${walletAddress}&limit=50`, options)
-			.then(response => response.json())
-			.then(response => response?.assets)
-			.catch(err => console.error(err));
+
+		return new Promise( res => res( dummyRobotNFTs ) );
+
+		return fetch( `https://api-testnet.aspect.co/api/v0/assets?contract_address=${robotsNFTsContractAddress}&owner_address=${walletAddress}&limit=50`, options )
+			.then( response => response.json() )
+			.then( response => response?.assets )
+			.catch( err => console.error( err ) );
 	};
+
+export const callRaceContract = ( wallet, entrypoint, calldata ) =>
+	wallet.provider.callContract( {contractAddress: raceContractAddress, entrypoint, calldata} );
+
+export const getRobotsInRace = wallet =>
+	callRaceContract( wallet, 'MakeRobotsArray' )
+		.then( ( {result} ) => result.map( id => `${+ id}` ) );
+
+export const getOpenRaceSlotsCount = wallet =>
+	callRaceContract( wallet, 'getSignUpCount' )
+		.then( ( {result} ) => {
+			return defaultSpacesInRace - result[0];
+		} );
+
+export const submitRobotToRace = ( wallet, robotId ) =>
+	callRaceContract( wallet, 'SubmitRobot', [robotId] )
+		.then( response => response )
+		.catch( e => {
+			console.error( e )
+		} );
