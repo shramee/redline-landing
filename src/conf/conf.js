03d910ee3b656ec60e550c0f3f3a6d1bc59b105a
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-08-23 02:44:30 +0800

    Config directory `conf`

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,6 @@
-import youtube from './assets/youtube-brands.svg';
-import tiktok from './assets/tiktok-brands.svg';
+import youtube from '../assets/youtube-brands.svg';
+import tiktok from '../assets/tiktok-brands.svg';
+import _abi_raceContract from './abi_raceContract.json';
 
 export const
 	twitchURL                 = 'https://player.twitch.tv/?channel=redlinedao&parent=' + window.location.host.split( ':' )[0],
@@ -20,3 +21,4 @@ export const
 	robotsNFTsContractAddress = '0x044915126b3736c22fc04be736caf0813451617fc562dc727d164361b81358a9',
 	raceContractAddress       = '0x05f940ac40c4aced54b371a00087e14d793a60813b80180f17cbb585e78ad14d';
 
+export const abi_raceContract = _abi_raceContract;
