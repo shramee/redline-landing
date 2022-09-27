659f5131b2ce505d0ed1a8b9ae8ca580f4142621
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-28 04:15:55 +0800

    Team scene

--- src/pages/Home/TeamScene.js
+++ src/pages/Home/TeamScene.js
@@ -13,7 +13,7 @@ import argent from '../../assets/partners/argent-x.svg';
 import aspect from '../../assets/partners/aspect.png';
 import cartridge from '../../assets/partners/cartridge.svg';
 import chainlink from '../../assets/partners/chainlink.png';
-import mintsquare from '../../assets/partners/mintsquare.png';
+import mintsquare from '../../assets/partners/mintsquare.svg';
 import yagi from '../../assets/partners/yagi.svg';
 
 const GEOFF = 'https://picsum.photos/seed/PIERRE/300';
@@ -71,9 +71,9 @@ const partners = [
 
 export default function TeamScene() {
 	return <Scene id='team' bgChildren={''}>
-		<div className="flex flex-wrap flex-nowrap-ns mt6 pt5 relative">
+		<div className="flex flex-wrap flex-nowrap-ns mt6 pt5 relative w-100 mw9">
 			<HugeBgText style={{top: 0}}>Team</HugeBgText>
-			<h2 className="scene-title pv4 w-100 w5-ns tl-ns relative">Team</h2>
+			<h2 className="scene-title pv4 pr5 tl-ns relative">Team</h2>
 			<div className="relative w-100">
 				{team.map( member => <article className="dib w-100 w-50-m w-25-l br3 pa3 mv3 border-box">
 					<div className="">
@@ -85,10 +85,10 @@ export default function TeamScene() {
 				</article> )}
 			</div>
 		</div>
-		<div className="flex flex-wrap flex-nowrap-ns mt6 pt5 relative">
+		<div className="flex flex-wrap flex-nowrap-ns mt6 pt5 relative w-100 mw9">
 			<HugeBgText style={{top: 0}}>Partners</HugeBgText>
-			<h2 className="scene-title pv4 w5-ns tl-ns relative">Partners</h2>
-			<div className="relative w-auto f5">
+			<h2 className="scene-title pv4 pr4 tl-ns relative">Partners</h2>
+			<div className="relative f5">
 				{partners.map( ([partner, img]) =>
 						<img src={img} className="br2 h3-ns dib ma4 border-box" title={partner} /> )}
 			</div>
