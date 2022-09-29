38021fbc9f62dde968bd252973db7905d93ff559
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-29 15:37:09 +0800

    Mobile tweaks

--- src/components/TwoColumnLayout.js
+++ src/components/TwoColumnLayout.js
@@ -3,7 +3,7 @@ const snm = window.snm || new ScrollAndMouse();
 
 export default function( {visual, flip, mid, visualStyle, visualClass, videoUrl, title, content} ) {
 	return <div ref={el => snm.addScrollTarget( el )} className={"flex pv4 pv5-m pv6-ns na3 flex-wrap " + (mid && 'items-center')}>
-		<div className={`w-100 w-40-m w-50-l pa3 ${flip && 'order-1'} ${visualClass}`} style={visualStyle}>
+		<div className={`w-100 w-40-m w-50-l pa3 ${flip && 'order-1-l'} ${visualClass}`} style={visualStyle}>
 			{visual}
 			{videoUrl && <video src={videoUrl} muted={true} autoPlay={true} loop={true} alt={title} className="w-100 br3"/>}
 		</div>
