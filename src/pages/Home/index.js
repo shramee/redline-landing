0da27271af4ffe2553e847ace8aa4e9d95524b61
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-29 14:11:11 +0800

    Adding Kenburns

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,20 @@ import HeroScene from './HeroScene';
 import RaceScene from './RaceScene';
 import AboutScene from './AboutScene';
 import FeatureScene from './FeatureScene';
+import KenBurns from '../../components/KenBurns';
 import TeamScene from './TeamScene';
 import RoadmapScene from './RoadmapScene';
+import empress from '../../assets/bg/empress.png';
+import city from '../../assets/bg/city.png';
 
 const Home = props =>
 	<article className="">
 		<HeroScene/>
 		<RaceScene/>
+		<KenBurns img={city}/>
 		<AboutScene/>
 		<FeatureScene/>
+		<KenBurns img={empress}/>
 		<TeamScene/>
 		<RoadmapScene/>
 	</article>;
