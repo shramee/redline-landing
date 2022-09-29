0eb103773fa737005487e7fd896b8805baaac825
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-29 14:08:46 +0800

    Team - layout, animations

--- src/pages/Home/TeamScene.js
+++ src/pages/Home/TeamScene.js
@@ -7,16 +7,19 @@ import David from '../../assets/team/David.png';
 import Alex from '../../assets/team/Alex.png';
 import Scott from '../../assets/team/Scott.png';
 import PA from '../../assets/team/PA.png';
+import You from '../../assets/team/you.png';
 import HugeBgText from "../../components/HugeBgText";
 
 import argent from '../../assets/partners/argent-x.svg';
 import aspect from '../../assets/partners/aspect.png';
 import cartridge from '../../assets/partners/cartridge.svg';
 import chainlink from '../../assets/partners/chainlink.png';
-import mintsquare from '../../assets/partners/mintsquare.svg';
+import mintsquare from '../../assets/partners/mintsquare.png';
 import yagi from '../../assets/partners/yagi.svg';
+import starkware from '../../assets/partners/starkware.png';
 
 const GEOFF = 'https://picsum.photos/seed/PIERRE/300';
+
 const snm = window.snm || new ScrollAndMouse();
 
 const team = [
@@ -55,9 +58,16 @@ const team = [
 		name: 'Pierre Alexis',
 		position: 'ADVISOR'
 	},
+	{
+		img: You,
+		name: 'You',
+		position: "Perfect role",
+		link: <a href='mailto:hello@isotropic.io'><svg className='h1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill='#fff' d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></a>,
+	},
 ];
 
 const partners = [
+	['Starkware', starkware],
 	['Argent-X', argent],
 	['Aspect', aspect],
 	['cartridge', cartridge],
@@ -66,31 +76,34 @@ const partners = [
 	['yagi', yagi],
 ]
 
-
-
+function staggerAnimate( el ) {
+	snm.staggerChildren( el, .1, .2 );
+	snm.addScrollTarget( el );
+}
 
 export default function TeamScene() {
 	return <Scene id='team' bgChildren={''}>
-		<div className="flex flex-wrap flex-nowrap-ns pt3 relative w-100 mw9">
-			<HugeBgText style={{top: 0}}>Team</HugeBgText>
-			<h2 className="scene-title pv4 pr5 tl-ns relative">Team</h2>
-			<div className="relative w-100">
-				{team.map( member => <article className="dib w-100 w-50-m w-25-l br3 pa3 mv3 border-box">
+		<HugeBgText ref={el => snm.addScrollTarget( el )}>Team</HugeBgText>
+		{/*<HugeBgText style={{top: 0}}>Partners</HugeBgText>*/}
+		<div className="pt3 relative w-100 mw9 tc">
+			<h2 ref={el => snm.addScrollTarget( el )} className="scene-title heading-anim pv4 pr5 tl-ns relative">Team</h2>
+			<div ref={el => staggerAnimate( el )} className="relative w-100 mw8 center">
+				{team.map( ( {link, img, name, position}) => <article className="dib v-top w-100 w-50-m w-25-l br3 pa3 mw5 mv3 border-box fadeInUp">
 					<div className="">
-						<img src={member.img} className="br2 w4 dib"
-								 title={member.name} />
-						<h3 className="f3 mt2 white-80 mb2 tracked">{member.name}</h3>
-						<span className="f5 fw4 white-50 mt0">{member.position}</span>
+						<img src={img} className="br2 w4 dib"
+								 title={name} />
+						<h3 className="f3 mt2 white-80 mb2 tracked">{name}</h3>
+						<span className="f5 fw4 white-50 mt0">{position}</span>
+						{link && <div>{link}</div>}
 					</div>
 				</article> )}
 			</div>
 		</div>
-		<div className="flex flex-wrap flex-nowrap-ns mt6 pt5 relative w-100 mw9">
-			<HugeBgText style={{top: 0}}>Partners</HugeBgText>
-			<h2 className="scene-title pv4 pr4 tl-ns relative">Partners</h2>
-			<div className="relative f5">
-				{partners.map( ([partner, img]) =>
-						<img src={img} className="br2 h3-ns dib ma4 border-box" title={partner} /> )}
+		<div className="mt6 pt5 relative w-100 mw9 tc">
+			<h2 ref={el => snm.addScrollTarget( el )} className="scene-title heading-anim pv4 pr4 tl-ns relative">Partners</h2>
+			<div ref={el => staggerAnimate( el )} className="relative f5 o-70">
+				{partners.map( ([partner, img], i) =>
+						<img src={img} className={`br2 mh4-l h4 pt5 dib ph4 pb3 border-box fadeInUp ${partner === 'yagi' && 'pb4'}`} title={partner} /> )}
 			</div>
 		</div>
 	</Scene>;
