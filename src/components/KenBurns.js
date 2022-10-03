e84ec82fa577ef7d26050b5565e6d3c88b23f132
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-03 19:54:26 +0800

    Removing full width images

--- src/components/KenBurns.js
+++ src/components/KenBurns.js
@@ -1,7 +1,5 @@
 export default function KenBurns( {img}) {
-	return <div className="w-100 db ken-burns" style={{
-		paddingBottom: '',
-	}}>
+	return <div className="w-100 db ken-burns" style={{}}>
 		<div className="ken-burns-img" style={{
 			backgroundImage: `url(${img})`,
 		}}></div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,20 +2,21 @@ import HeroScene from './HeroScene';
 import RaceScene from './RaceScene';
 import AboutScene from './AboutScene';
 import FeatureScene from './FeatureScene';
-import KenBurns from '../../components/KenBurns';
 import TeamScene from './TeamScene';
 import RoadmapScene from './RoadmapScene';
-import empress from '../../assets/bg/empress.png';
-import city from '../../assets/bg/city.png';
+
+//import KenBurns from '../../components/KenBurns';
+//import empress from '../../assets/bg/empress.png';
+//import city from '../../assets/bg/city.png';
 
 const Home = props =>
 	<article className="">
 		<HeroScene/>
 		<RaceScene/>
-		<KenBurns img={city}/>
+		{/*<KenBurns img={city}/>*/}
 		<AboutScene/>
 		<FeatureScene/>
-		<KenBurns img={empress}/>
+		{/*<KenBurns img={empress}/>*/}
 		<TeamScene/>
 		<RoadmapScene/>
 	</article>;
