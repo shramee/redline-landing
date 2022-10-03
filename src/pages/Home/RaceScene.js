068ef2c38371693508bb6bfee7ceb9669e689c77
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-10-03 20:10:21 +0800

    Roadmap and Race scene tweaks

--- src/pages/Home/RaceScene.js
+++ src/pages/Home/RaceScene.js
@@ -125,63 +125,66 @@ export default class RaceScene extends Component {
 		return `//redline-dao.github.io/data/flags/w80/${countryCode}.png`;
 	}
 
-	render() {
+	renderStream() {
+		return <div className='relative z-1'>
+			<h2 ref={el => snm.addScrollTarget( el )} className='scene-title anims heading-anim'>
+				<span className="live-blink clr v-mid" style={{marginRight: '.3em', fontSize: '1.4em',}}>&bull;</span>
+				Live now
+			</h2>
+			<div
+				className="aspect-ratio aspect-ratio--16x9 br2 overflow-hidden race-live-stream">
+				{
+					'localhost' === window.location.hostname ?
+						<img className='aspect-ratio--object' src={livestreamScreenShot} alt=""/> :
+						<iframe className="aspect-ratio--object cover" allowFullScreen scrolling="no"
+						title='Live twitch feed' height="450" width="800" src={twitchURL}/>
+				}
+			</div>
+		</div>;
+	}
+
+	renderUpcoming() {
 		const {races, tracks} = this.state;
-		return <Scene id='races' className='bg-chart'>
-			{/*{subHeading( {marginBottom: '-.4em'} )}*/}
-			<div className="w-100 w-80-l mw9">
-				<div className='relative z-1 mt5'>
-					<h2 ref={el => snm.addScrollTarget( el )} className='scene-title anims heading-anim'>
-						<span className="live-blink clr v-mid" style={{marginRight: '.3em', fontSize: '1.4em',}}>&bull;</span>
-						Live now
-					</h2>
-					<div
-						className="aspect-ratio aspect-ratio--16x9 br2 overflow-hidden race-live-stream">
-						{/*<img className='aspect-ratio--object' src={livestreamScreenShot} alt=""/>*/}
-						<iframe className="aspect-ratio--object cover" frameBorder="0" allowFullScreen scrolling="no"
-										title='Live twitch feed' height="450" width="800" src={twitchURL}/>
-					</div>
-				</div>
-				<div className='relative z-1 mt5'>
+		return <div className='relative z-1 mt5'>
 
-					<h2 ref={el => snm.addScrollTarget( el )}
-							className='dib scene-title text-bg anims heading-anim'
-					>
-						Upcoming Races
-					</h2>
-					<a href="//alpha.redline.game" className="f6 copy-font btn fl fr-ns mt3 mt0-ns">All races</a>
+			<h2 ref={el => snm.addScrollTarget( el )}
+					className='dib scene-title text-bg anims heading-anim'
+			>
+				Upcoming Races
+			</h2>
+			<a href="//alpha.redline.game" className="f6 copy-font btn fl fr-ns mt3 mt0-ns">All races</a>
 
-					<div className="races-wrap flex flex-wrap items-center mv3 nl3 nr3 cb" ref={el => {
-						this.stagger( el );
-						snm.addScrollTarget( el )
-					}}>
-						{races && races.map( (race, i) => {
-							if ( !race || !race.track ) {
-								return null;
-							}
-							const img = this.getTrackImage( race.track );
-							return <div key={i} className='w-100 w-50-m w-25-l o-70 anims fadeInUp'>
-								<div className="ma3 race pa2 bg br2">
-									<h3 style={{fontSize: '1.2rem'}} className='mb1 lh-solid ma1'>
-										<img className='v-mid w2 mr2 br1' src={this.getTrackCountryImage( race.track )}/>
-										{race.track.name}
-									</h3>
-									<small className="fl ma1">{moment( race.time ).fromNow()}</small>
-									<small className="ma1 br3 lh-copy fr">
-										<svg className='w1 v-mid mr1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-											<path fill='currentColor'
-														d="M400 0H176c-26.5 0-48.1 21.8-47.1 48.2c.2 5.3 .4 10.6 .7 15.8H24C10.7 64 0 74.7 0 88c0 92.6 33.5 157 78.5 200.7c44.3 43.1 98.3 64.8 138.1 75.8c23.4 6.5 39.4 26 39.4 45.6c0 20.9-17 37.9-37.9 37.9H192c-17.7 0-32 14.3-32 32s14.3 32 32 32H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H357.9C337 448 320 431 320 410.1c0-19.6 15.9-39.2 39.4-45.6c39.9-11 93.9-32.7 138.2-75.8C542.5 245 576 180.6 576 88c0-13.3-10.7-24-24-24H446.4c.3-5.2 .5-10.4 .7-15.8C448.1 21.8 426.5 0 400 0zM48.9 112h84.4c9.1 90.1 29.2 150.3 51.9 190.6c-24.9-11-50.8-26.5-73.2-48.3c-32-31.1-58-76-63-142.3zM464.1 254.3c-22.4 21.8-48.3 37.3-73.2 48.3c22.7-40.3 42.8-100.5 51.9-190.6h84.4c-5.1 66.3-31.1 111.2-63 142.3z"/>
-										</svg>
-										<span style={{letterSpacing: '1px'}}>ETH</span>&nbsp;{race.prize.ether}</small>
-									<div className='cb br1 aspect-ratio--16x9 race-track-image dotted-overlay-after'
-											 style={{background: `center/cover url(${img})`}}>
-										<a href="//alpha.redline.game" className="btn nt4 absolute-btn ma-auto tracked">Join race</a>
-									</div>
-								</div>
+			<div className="races-wrap flex flex-wrap items-center mt3 nl3 nr3 cb" ref={el => {
+				this.stagger( el );
+				snm.addScrollTarget( el )
+			}}>
+				{races && races.map( ( race, i ) => {
+					if ( !race || !race.track ) {
+						return null;
+					}
+					const img = this.getTrackImage( race.track );
+					return <div key={i} className='w-100 w-50-m w-25-l o-70 anims fadeInUp'>
+						<div className="ma3 race pa2 bg br2">
+							<h3 style={{fontSize: '1.2rem'}} className='mb1 lh-solid ma1'>
+								<img className='v-mid w2 mr2 br1' src={this.getTrackCountryImage( race.track )}/>
+								{race.track.name}
+							</h3>
+							<small className="fl ma1">{moment( race.time ).fromNow()}</small>
+							<small className="ma1 br3 lh-copy fr">
+								<svg className='w1 v-mid mr1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+									<path fill='currentColor'
+												d="M400 0H176c-26.5 0-48.1 21.8-47.1 48.2c.2 5.3 .4 10.6 .7 15.8H24C10.7 64 0 74.7 0 88c0 92.6 33.5 157 78.5 200.7c44.3 43.1 98.3 64.8 138.1 75.8c23.4 6.5 39.4 26 39.4 45.6c0 20.9-17 37.9-37.9 37.9H192c-17.7 0-32 14.3-32 32s14.3 32 32 32H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H357.9C337 448 320 431 320 410.1c0-19.6 15.9-39.2 39.4-45.6c39.9-11 93.9-32.7 138.2-75.8C542.5 245 576 180.6 576 88c0-13.3-10.7-24-24-24H446.4c.3-5.2 .5-10.4 .7-15.8C448.1 21.8 426.5 0 400 0zM48.9 112h84.4c9.1 90.1 29.2 150.3 51.9 190.6c-24.9-11-50.8-26.5-73.2-48.3c-32-31.1-58-76-63-142.3zM464.1 254.3c-22.4 21.8-48.3 37.3-73.2 48.3c22.7-40.3 42.8-100.5 51.9-190.6h84.4c-5.1 66.3-31.1 111.2-63 142.3z"/>
+								</svg>
+								<span style={{letterSpacing: '1px'}}>ETH</span>&nbsp;{race.prize.ether}</small>
+							<div className='cb br1 aspect-ratio--16x9 race-track-image dotted-overlay-after'
+									 style={{background: `center/cover url(${img})`}}>
+								<a href="//alpha.redline.game" className="btn nt4 absolute-btn ma-auto tracked">Join race</a>
 							</div>
-						} )}
+						</div>
 					</div>
-					{/*<div className="races-wrap flex flex-wrap items-center mv3 nl3 nr3">
+				} )}
+			</div>
+			{/*<div className="races-wrap flex flex-wrap items-center mv3 nl3 nr3">
 
 					{races && races.map( ( race, i ) => {
 						if ( !race || !race.track || i > 3 ) {
@@ -215,7 +218,7 @@ export default class RaceScene extends Component {
 				</div>*/}
 
 
-					{/*
+			{/*
 				<div className="races-wrap flex flex-wrap items-center mv3 nl3 nr3">
 					{races && races.map( race => {
 						if ( !race || !race.track ) {
@@ -247,7 +250,15 @@ export default class RaceScene extends Component {
 				</div>
 */}
 
-				</div>
+		</div>
+	}
+
+	render() {
+		return <Scene id='races' className='bg-chart'>
+			{/*{subHeading( {marginBottom: '-.4em'} )}*/}
+			<div className="w-100 w-80-l mw9">
+				{this.renderStream()}
+				{this.renderUpcoming()}
 			</div>
 		</Scene>;
 	}
--- src/pages/Home/RoadmapScene.js
+++ src/pages/Home/RoadmapScene.js
@@ -10,19 +10,19 @@ const itemHeight = 8;
 const items = [
 	[
 		<span>Proof of concept</span>,
-		<small className='db o-60 f5'>StrakNet &amp; Unreal integration</small>
+		<small className='db o-60 f5'>StarkNet &amp; Unreal integration</small>
 	],
 	[<span>Alpha Playtesting</span>,],
 	[<span>Backend account handling and direct fiat</span>,],
 	[<span>Sponsor System</span>, <small className='db o-60 f5'>Fully on chain and peer reviewed</small>],
-	[<span>Advanced racing systems & content</span>,],
-	[<span>Animation overhaul</span>,],
+//	[<span>Advanced racing systems & content</span>,],
+//	[<span>Animation overhaul</span>,],
 	[<span>StarkNet Mainnet Release</span>, '', 'roadmap-item-big'],
 	[
 		<span>Platform expansion: </span>,
 		<small className='db o-60 f5'>Webgl, mobile, other blockchain bridges</small>
 	],
-	[<span>Robot parts 2nd edition</span>,],
+//	[<span>Robot parts 2nd edition</span>,],
 	[<span>Betting system</span>,],
 	[<span>Future: </span>, <small className='db o-60 f5'>New games in the Redline universe</small>],
 ];
@@ -32,7 +32,7 @@ export default function RoadmapScene() {
 	const finishedMarker = <span className="ml2 dib bg-dark-blue br2 dib f6 lh-solid pa2 ttu v-mid">Done</span>;
 	return <Scene id='roadmap' className='overflow-hidden bg-chart' bgChildren={''}>
 		<div ref={el => snm.addScrollTarget( el )} className="w-100 w-80-l mw9">
-			<h2 ref={el => snm.addScrollTarget( el )} className="scene-title anims heading-anim mb4 pt5">Roadmap</h2>
+			<h2 ref={el => snm.addScrollTarget( el )} className="scene-title anims heading-anim mb4">Roadmap</h2>
 			<div className="roadmap-wrap">
 				<div className="roadmap-items">
 					<div className="roadmap-road">
